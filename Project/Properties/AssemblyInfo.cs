6f2b040555699a6bec4acfdbbc3b29683f650644
RussKie <russkie@gmail.com>
2018-11-24 23:49:24 +1100

    Align assembly information across the project

--- Project/Properties/AssemblyInfo.cs
+++ Project/Properties/AssemblyInfo.cs
@@ -5,15 +5,16 @@
 //     <version>$Revision$</version>
 // </file>
 
-using System;
 using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 
-[assembly: CLSCompliant(isCompliant: true)]
+[assembly: AssemblyCompany("ic#code")]
+[assembly: AssemblyCopyright("2000-2010 AlphaSierraPapa")]
+[assembly: NeutralResourcesLanguage("en-US")]
+
 [assembly: StringFreezing]
 
-[assembly: AssemblyTitle("ICSharpCode.TextEditor")]
+[assembly: AssemblyTitle("ICSharpCode.TextEditor v4.0.2.6466")]
 [assembly: AssemblyDescription("This is a GitExtensions fork of SharpDevelop .NET text editor control")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
