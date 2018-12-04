9383d35dd3db9e47ade1d165082daeec45c19359
RussKie <russkie@gmail.com>
2018-12-04 23:13:30 +1100

    Fix assembly info

--- Project/Properties/AssemblyInfo.cs
+++ Project/Properties/AssemblyInfo.cs
@@ -5,9 +5,11 @@
 //     <version>$Revision$</version>
 // </file>
 
+using System;
 using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("ic#code")]
 [assembly: AssemblyCopyright("2000-2010 AlphaSierraPapa")]
@@ -18,3 +20,26 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle("ICSharpCode.TextEditor v4.0.2.6466")]
 [assembly: AssemblyDescription("This is a GitExtensions fork of SharpDevelop .NET text editor control")]
 [assembly: AssemblyConfiguration("")]
+
+[assembly: AssemblyProduct("GitExtensions")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("2.99.90")]
+[assembly: AssemblyFileVersion("2.99.90")]
+[assembly: AssemblyInformationalVersion("3.00.rc2")]
+
+// Disable CLS compliance. See https://github.com/gitextensions/gitextensions/issues/4710
+[assembly: CLSCompliant(isCompliant: false)]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
